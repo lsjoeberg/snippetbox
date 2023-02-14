243b3e81409fd58c9811f0f2c6b2138363191200
Lars Sjöberg <lars.sjoberg@volvocars.com>
2023-02-14 12:55:12 +0100

    Add leveled logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,22 +4,39 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// App configuration via command-line.
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	// Setup loggers.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Create router.
 	mux := http.NewServeMux()
 
+	// Serve local files.
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// Create handlers.
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on %s\n", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	// Create a server and inject custom loggers.
+	srv := &http.Server{
+		Addr:     *addr,
+		Handler:  mux,
+		ErrorLog: errorLog,
+	}
+
+	// Start web server.
+	infoLog.Printf("Starting server on %s\n", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
