919871185f5ada565933137c8fa457140ed15796
Lars Sjöberg <lars.sjoberg@volvocars.com>
2023-02-14 14:14:54 +0100

    Isolate application routes to separate function

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -27,22 +27,10 @@ func main() {
 		errorLog: errorLog,
 	}
 
-	// Create router.
-	mux := http.NewServeMux()
-
-	// Serve local files.
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// Create handlers.
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	// Create a server and inject custom loggers.
 	srv := &http.Server{
 		Addr:     *addr,
-		Handler:  mux,
+		Handler:  app.routes(),
 		ErrorLog: errorLog,
 	}
 
---
+++ cmd/web/routes.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	// Create router.
+	mux := http.NewServeMux()
+
+	// Serve local files.
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Create handlers.
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	return mux
+}
