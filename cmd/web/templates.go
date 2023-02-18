a3200cc22fc878d50113d6b9087acd6b0d613238
Lars Sjöberg <lars.sjoberg@volvocars.com>
2023-02-18 14:05:41 +0100

    Display dates as human readable

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -21,6 +21,14 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{CurrentYear: time.Now().Year()}
 }
 
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
 	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
 	if err != nil {
@@ -32,7 +40,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		name := filepath.Base(page)
 
 		// Parse the base template into a template set.
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
