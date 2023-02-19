3e6229046a4db28549011000b4e4f7ebc8a14be0
Lars Sjöberg <lars.sjoberg@volvocars.com>
2023-02-19 17:59:49 +0100

    Add user model

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -2,4 +2,8 @@ package models
 
 import "errors"
 
-var ErrNoRecord = errors.New("models: no matching record")
+var (
+	ErrNoRecord           = errors.New("models: no matching record")
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	ErrDuplicateEmail     = errors.New("models: duplicate email")
+)
